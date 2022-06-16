6f5815c84dca046589942a892589509eeac64fb2
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-15 23:58:03 -0300

    Tabela exemplo criada

--- src/app/tabela-dados/tabela-dados.component.ts
+++ src/app/tabela-dados/tabela-dados.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tabela-dados.component.html',
   styleUrls: ['./tabela-dados.component.css']
 })
-export class TabelaDadosComponent implements OnInit {
+export class TabelaDadosComponent {
 
   constructor() { }
+  
+  elements: any = [
+    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
+    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
+    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
+  ];
 
-  ngOnInit(): void {
-  }
+  headElements = ['ID', 'First', 'Last', 'Handle'];
 
 }
