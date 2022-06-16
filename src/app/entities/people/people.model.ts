473e90a4f78236e4a03cbbd862e2be4a552d7d8d
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-16 01:16:42 -0300

    modelo de people

---
+++ src/app/entities/people/people.model.ts
@@ -0,0 +1,25 @@
+export interface People extends Object{
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;  
+    birth_year: string;  
+    gender: string;  
+    homeworld: string;  
+    films: string[];  
+    species: string[];  
+    vehicles: string[];  
+    starships: string[];  
+    created: string;  
+    edited: string;  
+    url: string;  
+}
+
+export interface PeopleResult {
+    count: number;
+    next: string;
+    previous: null;
+    results: People[];
+}
