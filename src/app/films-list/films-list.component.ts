2081ab46aa5e990da2316f637055c4f1536c7ed5
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-23 14:38:32 -0300

    Outra forma de fazer o getListaDeFilmes

--- src/app/films-list/films-list.component.ts
+++ src/app/films-list/films-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Films } from '../entities/films/films.model';
+import { Films, FilmsResult } from '../entities/films/films.model';
 import { FilmsService } from '../entities/films/films.service';
 
 @Component({
@@ -12,23 +12,41 @@ export class FilmsListComponent implements OnInit {
   titleFilm: string = '';
   listFilms: string[] | undefined;
   titlesFilms: string[] = [];
+  // ---------------------
+  listOfFilms: Films[] = [];
+  listOfTitles: string[] = [];
 
   constructor(private filmsService: FilmsService) { }
   
   ngOnInit(): void {
     if(this.films != undefined){
       this.listFilms = this.films.split(',');
-      this.getAllTitles(this.listFilms)
+      this.getAllFilms()
+      // this.getTitlesByUrl(this.listFilms)
     }
   }
 
-  async getAllTitles(listFilms: string[]){
+  async getTitlesByUrl(listFilms: string[]){
+    console.time("api");
     listFilms.forEach(urlFilm => {
       this.filmsService.getFilmById(urlFilm).subscribe(
         (film: Films) => {
           this.titlesFilms.push(film.title);
         } );
-    });
+      });
+        console.timeEnd("api");
+  }
+
+  getAllFilms() {
+    console.time("filter");
+    this.filmsService.getAllFilms().subscribe(
+      (listOfFilms: FilmsResult) => {
+        this.listOfFilms = listOfFilms.results.filter((el) => {
+          return this.listFilms?.includes(el.url)
+        })
+        this.listOfTitles = this.listOfFilms.map((el) => el.title)
+      } );
+        console.timeEnd("filter");
   }
 
 }
