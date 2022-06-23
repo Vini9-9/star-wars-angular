13cf9dd8bcc43109478aee5ed448529127a42996
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-23 15:28:14 -0300

    Criação da entidade

---
+++ src/app/entities/users/users.model.ts
@@ -0,0 +1,6 @@
+export interface User extends Object{
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
---
+++ src/app/entities/users/users.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './users.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    userUrl = 'http://localhost:8080/api/user';
+
+    getAllUsers(){
+    return this.http.get<any>(this.userUrl);
+    }
+
+    // public saveUser(usuario: User): Observable<any> {
+    //     console.log(usuario);
+    //     const body = JSON.stringify({nomeUsuario: usuario.nomeUsuario, idadeUsuario: usuario.idadeUsuario});
+    
+    //     return this.http.request('POST', this.userUrl, {
+    //       headers: new HttpHeaders({
+    //         'Content-Type': 'application/json'
+    //       }),
+    //       body: body
+    //     })
+    //   }
+    
+      public updateUser(usuario: User): Observable<any> {
+        const body = JSON.stringify(usuario);
+    
+        return this.http.request('PUT', this.userUrl + '/' + usuario.id, {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+          }),
+          body: body
+        })
+      }
+
+
+}
